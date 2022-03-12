c8f92fd4a2e92154a1a3d002e0bf61bf37347ef9
carolina <carorua19@gmail.com>
2022-03-12 08:38:01 -0500

    Envio taller de Javascript

---
+++ Taller3DianaCarolinaTabordaRua/javascript.js
@@ -0,0 +1,165 @@
+// TALLER DIANA CAROLINA TABORDA RUA 
+
+// 1-
+
+/* let EdadPepito=prompt("Introduce la edad de Pepito");
+   let EdadJuanita=prompt("Introduce la edad de Juanita");
+
+        if(parseInt(EdadPepito)>parseInt(EdadJuanita)){
+        alert("Pepito es mayor que Juanita");
+
+        }
+
+        else if(parseInt(EdadPepito)<(EdadJuanita)){
+        alert("Juanita es mayor que Pepito");
+        }
+
+*/
+
+// 2-
+
+/* let Student1, Student2, Student3, Student4, Student5;
+        let Promedio;
+        Student1=Number(prompt("Ingrese su nota del Alumno 1"));
+        Student2=Number(prompt("Ingrese su nota del Alumno 2"));
+        Student3=Number(prompt("Ingrese su nota del Alumno 3"));
+        Student4=Number(prompt("Ingrese su nota del Alumno 4"));
+        Student5=Number(prompt("Ingrese su nota del Alumno 5"));
+
+        Promedio= ((Student1+Student2+Student3+Student4+Student5)/5);
+
+        alert("La nota promedio es: " + (Promedio));
+*/
+
+
+// 3-
+
+/*let clave = "dom";
+let claveSolicitada = prompt("Digite la clave");
+
+
+if(clave === claveSolicitada){
+   alert("Bienvenidos al club de los magios");
+}
+
+else {
+    alert("No puedes ingresar, no eres miembro");
+}
+
+*/
+
+// 4-
+
+ /* 
+ let vacuna=prompt("Responda sí o no si está vacunado");
+            if (vacuna.toLowerCase() == ("si")){
+            alert("Bienvenido");
+            }
+   
+            else{
+            alert("Por favor ve a vacunarte")
+            } 
+
+*/
+
+// 5- 
+/* 
+   
+    let anio = parseInt(prompt("Por favor ingrese su año de nacimiento completo"));
+    if (anio <= 2022) { 
+        alert("Su edad es de " + (2022 - anio) + " años");
+    }
+
+    else {
+        alert("Por favor ingrese un año menor al actual");
+    }
+
+*/
+
+// 6-
+/*
+
+let capitales = parseInt(prompt("Digite el numero del pais el cual le gustaria saber la capital: \n 1. Colombia \n 2. Argentina \n 3. Peru \n 4. Japon \n 5. Alemania"));
+
+    switch(capitales){
+
+    case 1:
+        alert("Bogota");
+        break;
+
+    case 2:
+         alert("Buenos Aires");
+         break;
+
+    case 3:
+        alert("Lima");
+        break;
+
+    case 4:
+        alert("Tokio");
+        break;
+
+    case 5:
+        alert("Berlin");
+        break;
+
+    default:
+        alert("Error, ingrese del 1 al 5, gracias");
+        break;
+}
+
+*/
+
+// 7- 
+
+/*
+let tabla=5;
+let limite=100;
+
+
+for (let i=1; i<=limite; i++){
+    let multi=tabla*i;
+    document.write("tabla del "+tabla + " x " + i + " = " + multi + "<br>");
+
+}
+*/
+
+// 8-
+/*
+let words = parseInt(prompt("¿Cuál palabra te gustaría traducir? : \n 1. Olvido  \n 2. Cielo \n 3. Mar \n 4. Lobo \n 5. Sentir"));
+
+switch(words){
+
+case 1:
+    alert("Oblivion");
+    break;
+
+case 2:
+     alert("Heaven");
+     break;
+
+case 3:
+    alert("Sea");
+    break;
+
+case 4:
+    alert("Wolf");
+    break;
+
+case 5:
+    alert("Feel");
+    break;
+
+default:
+    alert("Opción no válida");
+    break;
+}
+*/
+
+
+
+
+
+
+
+
